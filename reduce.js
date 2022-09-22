d2b644191332f391c45b1309ba39767b2f68eb5d
David Guillaumant-Mergler <47602483+visionspider@users.noreply.github.com>
2022-09-22 09:01:45 -0400

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -2,18 +2,18 @@
 
 const arr = ["apples ", "are delicious ", ", yum!"];
 
-const reduceResult = arr.reduce((sum, number, index, array) => {
+const reduceResult = arr.reduce((sum, string, index, array) => {
   console.log(
     "index = ",
     index,
     "sum = ",
     sum,
-    "number = ",
-    number,
-    "accumulator + number = ",
-    sum + number
+    "string = ",
+    string,
+    "sum + string = ",
+    sum + string
   );
-  return sum + number; //the sum parameter is taking the return value on each iteration.
+  return sum + string; //the sum parameter is taking the return value on each iteration.
 }, "All "); // initial value (if initial value is not set then initial value is taken from array at index 0)
 
 console.log(reduceResult);
